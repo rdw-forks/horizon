53212f0f6736e969e2f608058ac71c82e461757b
Sagun Khosla <sagunxp@gmail.com>
2021-02-18 11:58:10 +0530

    Travis test

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -31,23 +31,6 @@ function vercomp () {
 
 set -x
 
-# Cmake version upgrade. (Not Required currently, also incomplete)
-# cmake_version="$(cmake --version | grep -o -E "([0-9]+).([0-9]+).([0-9]+)")"
-# req_cmake_version="3.13.2"
-# vercomp "$cmake_version" "$req_cmake_version"
-# if [ $? -eq 2 ]; then
-# 	rm -rf /usr/bin/cmake;
-# 	export PATH="$PATH:/usr/local/bin"
-# 	echo "CMake version '$cmake_version' does not satisfy '$req_cmake_version', installing latest...";
-# 	wget https://github.com/Kitware/CMake/releases/download/v3.13.4/cmake-3.13.4.tar.gz;
-# 	tar -xvf cmake-3.13.4.tar.gz >/dev/null;
-# 	cd cmake-3.13.4;
-# 	echo "Silently building and installing CMake...";
-# 	./bootstrap >/dev/null;
-# else
-# 	echo "CMake version $cmake_version is installed and compatible.";
-# fi
-
 pushd .
 
 sudo apt-get -y update;
@@ -98,22 +81,6 @@ else
 	echo "Sqlpp11-connector-mysql already exists, skipping installation...";
 fi
 
-sudo apt-get -y install libreadline-dev liblua5.3-dev zlib1g-dev;
-
-mysql_version="$(mysql --version | grep -o -E "([0-9]+).([0-9]+).([0-9]+)")"
-req_mysql_version="8.0.15"
-vercomp "$mysql_version" "$req_mysql_version"
-
-if [ $? -eq 2 ]; then
-	echo "MySQL 8.0.15 is required but not found, installing latest..."
-	pushd /tmp;
-	wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb;
-	sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb;
-	popd;
-	sudo apt-get -y update;
-	sudo apt-get -y install mysql-server mysql-client libmysqlcppconn-dev;
-else
-	echo "MySQL 8.0.15 is already installed."
-fi
+sudo apt-get -y install libreadline-dev liblua5.3-dev zlib1g-dev mariadb-server mariadb-client;
 
 popd;
