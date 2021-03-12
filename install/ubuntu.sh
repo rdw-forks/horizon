5535e4601caabb48d0ce2a6a6ea9412951d80994
Smokex <sagunxp@gmail.com>
2021-03-11 23:30:30 -0800

    Travis test

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,8 +1,7 @@
 pushd .
 
-sudo apt-get remove cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.20.0-rc4/cmake-3.20.0-rc4-linux-x86_64.sh
-./cmake-3.20.0-rc4-linux-x86_64.sh
+CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v3.20.0-rc4/cmake-3.20.0-rc4-linux-x86_64.tar.gz"
+mkdir cmake && travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
 
 sudo apt-get -y install libreadline-dev liblua5.3-dev libboost-all-dev zlib1g-dev mariadb-server mariadb-client;
 
