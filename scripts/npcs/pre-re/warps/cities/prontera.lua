0a53067a4adc7a4f4c4814f4212f3805343306a1
Sinji Prasetio <sinjiprasetio@outlook.com>
2023-12-23 14:54:44 +0700

    Added renewal and pre renewal warps to prontera to match Royal Banquet.

---
+++ scripts/npcs/pre-re/warps/cities/prontera.lua
@@ -0,0 +1,56 @@
+---------------------------------------------------------------------------
+--       _   _            _                        --
+--      | | | |          (_)                       --
+--      | |_| | ___  _ __ _ _______  _ __          --
+--      |  _  |/ _ \| '__| |_  / _ \| '_  \        --
+--      | | | | (_) | |  | |/ / (_) | | | |        --
+--      \_| |_/\___/|_|  |_/___\___/|_| |_|        --
+---------------------------------------------------------------------------
+-- This file is part of Horizon (c).
+-- Copyright (c) 2018 Horizon Dev Team.
+--
+-- Base Author - Sagun K. (sagunxp@gmail.com)
+--
+-- This library is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this library.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------------
+--- Prontera Warp Definitions File.
+---------------------------------------------------------------------------
+
+Warp("prt005", "prontera", 156, 360, "s:move_to_map('prt_castle', 102, 20)", 2)
+Warp("prtca01", "prt_castle", 102, 129, "s:move_to_map('prt_castle', 102, 143)", 2)
+Warp("prtca01-1", "prt_castle", 102, 140, "s:move_to_map('prt_castle', 102, 126)", 2)
+Warp("prtca001", "prt_castle", 102, 16, "s:move_to_map('prontera', 156, 356)", 2)
+Warp("prtca002", "prt_castle", 102, 181, "s:move_to_map('prt_gld', 159, 28)", 2)
+Warp("prtca02", "prt_castle", 102, 73, "s:move_to_map('prt_castle', 102, 91)", 2)
+Warp("prtca02-1", "prt_castle", 102, 88, "s:move_to_map('prt_castle', 102, 70)", 2)
+Warp("prtca03", "prt_castle", 113, 107, "s:move_to_map('prt_castle', 134, 107)", 2)
+Warp("prtca04", "prt_castle", 121, 29, "s:move_to_map('prt_castle', 148, 29)", 2)
+Warp("prtca03-1", "prt_castle", 130, 107, "s:move_to_map('prt_castle', 110, 107)", 2)
+Warp("prtca05", "prt_castle", 135, 153, "s:move_to_map('prt_castle', 167, 145)", 2)
+Warp("prtca04-1", "prt_castle", 144, 29, "s:move_to_map('prt_castle', 117, 29)", 2)
+Warp("prtca06", "prt_castle", 149, 113, "s:move_to_map('prt_castle', 175, 113)", 2)
+Warp("prtca05-1", "prt_castle", 164, 145, "s:move_to_map('prt_castle', 132, 153)", 2)
+Warp("prtca07", "prt_castle", 170, 138, "s:move_to_map('prt_castle', 176, 118)", 2)
+Warp("prtca06-1", "prt_castle", 172, 113, "s:move_to_map('prt_castle', 146, 113)", 2)
+Warp("prtca07-1", "prt_castle", 176, 121, "s:move_to_map('prt_castle', 170, 141)", 2)
+Warp("prtca08", "prt_castle", 28, 121, "s:move_to_map('prt_castle', 40, 141)", 2)
+Warp("prtca09", "prt_castle", 31, 113, "s:move_to_map('prt_castle', 58, 113)", 2)
+Warp("prtca08-1", "prt_castle", 40, 138, "s:move_to_map('prt_castle', 28, 118)", 2)
+Warp("prtca10", "prt_castle", 45, 145, "s:move_to_map('prt_castle', 72, 153)", 2)
+Warp("prtca09-1", "prt_castle", 54, 113, "s:move_to_map('prt_castle', 27, 113)", 2)
+Warp("prtca11", "prt_castle", 59, 29, "s:move_to_map('prt_castle', 85, 29)", 2)
+Warp("prtca10-1", "prt_castle", 68, 153, "s:move_to_map('prt_castle', 42, 145)", 2)
+Warp("prtca12", "prt_castle", 75, 107, "s:move_to_map('prt_castle', 95, 107)", 2)
+Warp("prtca11-1", "prt_castle", 82, 29, "s:move_to_map('prt_castle', 56, 29)", 2)
+Warp("prtca12-1", "prt_castle", 92, 107, "s:move_to_map('prt_castle', 72, 107)", 2)
