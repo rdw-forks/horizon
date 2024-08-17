b8c9b8d145a6788cb1ae54fe392fddf86f5096b2
Sagun Khosla <sagunxp@gmail.com>
2024-08-18 02:59:56 +0530

    Collect garbage from lua before exiting and release state.

--- src/Server/Zone/Script/ScriptManager.cpp
+++ src/Server/Zone/Script/ScriptManager.cpp
@@ -57,6 +57,8 @@ _resource_manager(PrimaryResource(RESOURCE_PRIORITY_PRIMARY, std::make_shared<s_
 
 ScriptManager::~ScriptManager()
 {
+	if (_lua_state != nullptr)
+		_lua_state.reset();
 }
 
 void ScriptManager::initialize(int segment_number)
@@ -90,9 +92,9 @@ void ScriptManager::finalize()
 		}
 	}
 
-	if (_lua_state != nullptr)
-		_lua_state.reset();
-		
+	_lua_state->collect_garbage();
+	_lua_state->release();
+	
 	_script_files.clear();
 
 	_is_finalized.exchange(true);
