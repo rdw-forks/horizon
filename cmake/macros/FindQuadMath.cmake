d187011d7f2e0b0e60ec672280b0ed16a92b8685
Sagun Khosla <sagunxp@gmail.com>
2024-08-22 10:18:44 +0530

    Add quadmath library for linkage with Boost.CharConv and Boost.Mysql on linux systems

---
+++ cmake/macros/FindQuadMath.cmake
@@ -0,0 +1,38 @@
+###################################################
+#       _   _            _                        #
+#      | | | |          (_)                       #
+#      | |_| | ___  _ __ _ _______  _ __          #
+#      |  _  |/ _ \| '__| |_  / _ \| '_  \        #
+#      | | | | (_) | |  | |/ / (_) | | | |        #
+#      \_| |_/\___/|_|  |_/___\___/|_| |_|        #
+###################################################
+# This file is part of Horizon (c).
+# Copyright (c) 2018 Horizon Dev Team.
+#
+# Base Author - Sagun K. (sagunxp@gmail.com)
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+###################################################
+
+find_library(QUADMATH_LIBRARY
+	NAMES quadmath
+	PATHS
+		/usr/lib/gcc/x86_64-linux-gnu/10/
+	)
+
+if (QUADMATH_LIBRARY)
+	message(STATUS "Found QuadMath Library: ${QUADMATH_LIBRARY} (Required for Boost.CharConv / Boost.MySQL)")
+else()
+	message(FATAL_ERROR "Could not find QuadMath Library (Required for Boost.CharConv / Boost.MySQL)")
+endif()
